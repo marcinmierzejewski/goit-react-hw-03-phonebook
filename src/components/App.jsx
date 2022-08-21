53c891b064b7c43f251ffcd0c25ad226c30d1c10
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-08-21 23:37:57 +0200

    add localStorage by used lifecycle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { ContactsList } from './contactsList/ContactsList';
 import { nanoid } from 'nanoid';
 import { SearchFilter } from './searchFilter/SearchFilter';
 import { Section } from './section/Section';
-import styles from './App.module.css'
+import styles from './App.module.css';
 
 export const INITIAL_STATE = {
   contacts: [],
@@ -13,6 +13,22 @@ export const INITIAL_STATE = {
 };
 
 export class App extends Component {
+  componentDidMount = () => {
+    let loadValues = JSON.parse(localStorage.getItem('LOCALSTORAGE_KEY'));
+    this.setState({
+      contacts: loadValues,
+    });
+  };
+
+  componentDidUpdate = () => {
+    try {
+      const initialState = JSON.stringify(this.state.contacts);
+      localStorage.setItem('LOCALSTORAGE_KEY', initialState);
+    } catch (error) {
+      console.error('Set state error: ', error.message);
+    }
+  };
+
   state = {
     ...INITIAL_STATE,
   };
@@ -20,8 +36,8 @@ export class App extends Component {
   addNewContact = ({ name, number }) => {
     const { contacts } = this.state;
 
-    if (contacts.find(cont => cont.name === name)){
-      alert(`${name} is already in contacts`)
+    if (contacts.find(cont => cont.name === name)) {
+      alert(`${name} is already in contacts`);
     } else {
       this.setState({
         contacts: [...contacts, { name, number, id: nanoid() }],
@@ -45,21 +61,21 @@ export class App extends Component {
   };
 
   render() {
-    const { wrapper } = styles
+    const { wrapper } = styles;
     return (
       <div className={wrapper}>
         <h1 style={{ textAlign: 'center' }}>React homework 2 phonebook</h1>
-        <Section title='Phonebook'>
+        <Section title="Phonebook">
           <PhoneBook newContact={this.addNewContact} />
         </Section>
-          
-        <Section title='Contacts'>
+
+        <Section title="Contacts">
           <SearchFilter searchByName={this.searchByName} />
           <ContactsList
             contacts={this.viewContacts()}
             deleteItem={this.deleteContact}
           />
-        </Section>       
+        </Section>
       </div>
     );
   }
